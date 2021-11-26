651c05c4696b0678a441e430c7f964a5a723f797
saidmsaif <saidmsaiff@gmail.com>
2021-11-26 20:00:52 +0600

    change section id

--- inc/plantex-options/home-page-options.php
+++ inc/plantex-options/home-page-options.php
@@ -1,11 +1,11 @@
 <?php
     CSF::createSection($option_prefix, array(
-        'title'         =>  __('Page', 'plantex'),
-        'id'            =>  'page'
+        'title'         =>  __('Home Page', 'plantex'),
+        'id'            =>  'home_page'
     ));
 
     CSF::createSection($option_prefix, array(
-        'parent'        =>  'page',
+        'parent'        =>  'home_page',
         'title'         =>  __('Hero Section', 'plantex'),
         'fields'        =>  array(
             array(
